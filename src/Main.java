f8f9b1e65d30c5512074974e1bd16b3b3e03d1f2
kevstevie <jianna442@gmail.com>
2022-10-29 17:17:41 +0900

    feat: add couting alphabets

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,21 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         String[] Numbers = new String[N];
+        int[] countAlphabets = new int[10];
+        boolean[] notZero = new boolean[10];
 
         for (int i = 0; i < N; i++) {
             Numbers[i] = sc.nextLine();
+        }
+
+        for (String str : Numbers) {
+            int len = str.length();
+            for (int i = 0; i < len; i++) {
+                if ( i == 0 ) {
+                    notZero[str.charAt(i - 'A')] = true;
+                }
+                countAlphabets[str.charAt(i - 'A')] += times(len - i);
+            }
         }
     }
 }
