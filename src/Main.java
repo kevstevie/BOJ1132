b8ff05fb389bebed403c97876a880c2a89b9f832
kevstevie <jianna442@gmail.com>
2022-10-29 17:10:49 +0900

    feat: add input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] Numbers = new String[N];
 
+        for (int i = 0; i < N; i++) {
+            Numbers[i] = sc.nextLine();
+        }
     }
 }
