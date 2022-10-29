04d4380b9cbb406aebdaf7beea7f4a7fae586cae
kevstevie <jianna442@gmail.com>
2022-10-29 17:36:19 +0900

    feat: add new Arrays for get result

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        String[] Numbers = new String[N];
+        String[] numbers = new String[N];
         int[] countAlphabets = new int[10];
         boolean[] notZero = new boolean[10];
+        Number[] counting = new Number[N];
 
         for (int i = 0; i < N; i++) {
-            Numbers[i] = sc.nextLine();
+            numbers[i] = sc.nextLine();
         }
 
-        for (String str : Numbers) {
+        for (String str : numbers) {
             int len = str.length();
             for (int i = 0; i < len; i++) {
                 if ( i == 0 ) {
@@ -21,6 +23,11 @@ class Main {
                 countAlphabets[str.charAt(i - 'A')] += times(len - i);
             }
         }
+
+        for (int i = 0; i < N; i++) {
+            counting[i] = new Number(countAlphabets[i], notZero[i]);
+        }
+        Arrays.sort(counting);
 
     }
 
